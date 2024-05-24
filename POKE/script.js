4fd366a3c6f7405b42ea06527f66c8937e15e615
Pierre-Gei <pierregeiguer@gmail.com>
2024-05-24 15:48:54 +0200

    POKE : new tables & responsive update

--- POKE/script.js
+++ POKE/script.js
@@ -1,9 +1,13 @@
 let choix = "locations";
 var locations = document.getElementById("locations");
+var location_table = document.getElementById("table_location");
 var lines = document.getElementsByClassName("line_select");
 var edges = document.getElementById("edges");
+var edges_table = document.getElementById("table_edges");
 var gateways = document.getElementById("gateways");
+var gateways_table = document.getElementById("table_gateway");
 var locationMap = document.getElementById("location_map");
+var table_title = document.getElementById("table_title");
 
 locations.addEventListener("click", function () {
     choixNavbar("locations");
@@ -35,16 +39,32 @@ function updateSelection(selected) {
         }
     }
     if (selected == "locations") {
+        location_table.style.display = "block";
+        edges_table.style.display = "none";
+        gateways_table.style.display = "none";
         lines[0].classList.add("line--active");
+        table_title.innerText = "Locations Inventory";
     }
     else if (selected == "edges") {
+        location_table.style.display = "none";
+        edges_table.style.display = "block";
+        gateways_table.style.display = "none";
         lines[1].classList.add("line--active");
+        table_title.innerText = "Edges Inventory";
     }
     else if (selected == "gateways") {
+        location_table.style.display = "none";
+        edges_table.style.display = "none";
+        gateways_table.style.display = "block";
         lines[2].classList.add("line--active");
+        table_title.innerText = "Gateways Inventory";
     }
     else if (selected == "location_map") {
+        location_table.style.display = "none";
+        edges_table.style.display = "none";
+        gateways_table.style.display = "none";
         lines[3].classList.add("line--active");
+        table_title.innerText = "Location Map";
     }
 }
 
